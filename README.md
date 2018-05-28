702cea2a419d14f9b1ba7efa7ba6e944ed00f78a
David Nunez <arizonatribe@gmail.com>
2018-05-27 20:09:21 -0700

    Added shapeStrictly. Added shapeline. Improved shapey so that it can handle non-objects as input values. Also wrapping applySpec field functions in tryCatch.

--- README.md
+++ README.md
@@ -8,31 +8,44 @@ A light, intuitve syntax for re-shaping objects in JavaScript, inspired by [Ramd
 npm install --save shapey
 ```
 
-### Usage
+### Default Usage
+
+In its default mode, shapey will "loosely" shape an input object. This means the spec shape you provide will merge over the input object.
 
 ```javascript
 import shape from 'shapey'
 
-const james = {
+const jimput = {
     morrison: 'jim',
     hendrix: 'jim',
-    carter: 'jim'
+    carter: 'jim',
+    world: 'jim'
 }
 
-shape({
-    hendrix: concat(__, 'mi'),
-    carter: concat(__, 'my'),
+const jimmify = shape({
+    hendrix: jim => jim + 'mi',
+    carter: jim => jim + 'my',
     dean: 'james',
-    jims: compose(map(join(', ')), toPairs, filter(regTest(/^jim/)))
-})(james)
+    world: jim => jim + 'my' + 'eat',
+    jims(allJims) {
+        return Object.entries(allJims).reduce(
+            (jimSurnames, [lastName, firstName]) => (
+                !/^jim/.test(firstName || '') ? jimSurnames : [jimSurnames, lastName].filter(Boolean).join(', ')
+            ), '')
+    }
+})
+
+jimmify(jimput)
 
 // {
 //  morrison: 'jim',
 //  hendrix: 'jimmi',
 //  carter: 'jimmy'
 //  dean: 'james',
-//  jims: ['morrison, jim', 'hendrix, jimmi', 'carter, jimmy']
+//  world: 'jimmy eat',
+//  jims: 'morrison, hendrix, carter'
 // }
+
 ```
 
 ### Explanation
@@ -43,3 +56,48 @@ To clarify what is happening in that code example:
 * Since props like `hendrix` and `carter` ARE specified in the spec, the `concat()` function is passed the original values for those props. This changes the original values to "jimmi" and "jimmy" by concatenating "mi" and "my" onto the original value of "jim".
 * Since a prop like `dean` DOESN'T exist on the original object (but is specified on the spec), its corresponding `compose()` function is passed the _entire_ input object (rather than just a single prop from the original input object).
 * Any prop (such as `jims`) which is _not_ a function has its value passed through onto the finished result (no operation is performed on the input object for a prop like this).
+
+
+### Strict Shaping
+
+Alternatively you can run "strict" shaping on an input object, which means the properties named in your spec will be the _only_ ones included in the final output.
+
+```javascript
+import {shapeStrictly} from 'shapey'
+
+const manyJims = {
+    morrison: 'jim',
+    hendrix: 'jim',
+    carter: 'jim',
+    gaffigan: 'jim',
+    carrey: 'jim',
+    beam: 'jim',
+    dammit: 'jim',
+    slim: 'jim',
+    henson: 'jim'
+}
+
+const jimsWhoLaugh = shapeStrictly({
+    carrey: jim => jim,
+    gaffigan: jim => jim
+})
+
+jimsWhoLaugh(manyJims)
+
+// {carrey: 'jim', gaffigan: 'jim'}
+
+
+const jimsWhoRock = shapeStrictly({
+    morrison: jim => jim,
+    hendrix: jim => jim + 'mi'
+})
+
+jimsWhoRock(manyJims)
+
+// {
+//  morrison: 'jim',
+//  hendrix: 'jimmi'
+// }
+```
+
+The difference between strict and loose shaping is _only_ the fields in your spec object will appear on the the final output. In loose mode however, all the shaped fields are merged on top of the original input.
--- test/index.js
+++ test/index.js
@@ -1,7 +1,18 @@
 import test from 'tape'
-import {__, concat, compose, map, join, toPairs, filter, test as regTest} from 'ramda'
-import shape from '../lib'
+import {__, concat, compose, map, head, join, length, pipe, toPairs, filter, test as regTest, sum} from 'ramda'
+import shape, {shapeStrictly, shapeLoosely, shapeline} from '../lib'
     
+const spec = {
+    hendrix: concat(__, 'mi'),
+    carter: concat(__, 'my'),
+    dean: 'james',
+    jims: compose(map(join(', ')), toPairs, filter(regTest(/^jim/)))
+}
+const inputObj = {
+    morrison: 'jim',
+    hendrix: 'jim',
+    carter: 'jim'
+}
 const specResult = {
     morrison: 'jim',
     hendrix: 'jimmi',
@@ -10,16 +21,7 @@ const specResult = {
 
 test('"shape" blends an object with a copy of itself transformed according to a spec', (t) => {
     t.deepEqual(
-        shape({
-            hendrix: concat(__, 'mi'),
-            carter: concat(__, 'my'),
-            dean: 'james',
-            jims: compose(map(join(', ')), toPairs, filter(regTest(/^jim/)))
-        }, {
-            morrison: 'jim',
-            hendrix: 'jim',
-            carter: 'jim'
-        }), {
+        shape(spec, inputObj), {
             ...specResult,
             dean: 'james',
             jims: ['morrison, jim', 'hendrix, jimmi', 'carter, jimmy']
@@ -30,5 +32,54 @@ test('"shape" blends an object with a copy of itself transformed according to a 
         {...specResult, morrison: 'Van'},
         'can overwrite an existing prop with a hard-coded value'
     )
+    t.deepEqual(
+        shapeLoosely({morrison: 'Van'}, specResult),
+        {...specResult, morrison: 'Van'},
+        'default export is synonymous with named export "shapeLoosely"'
+    )
+    t.end()
+})
+
+test('"shapeStrictly" creates a new object from only the fields in the spec', (t) => {
+    t.deepEqual(
+        shapeStrictly({
+            hendrix: jim => `${jim}mi`,
+            carter: jim => `${jim}my`,
+            dean: 'james',
+            jims(allJims) {
+                return Object.entries(allJims).reduce(
+                    (jimSurnames, [lastName, firstName]) => (
+                        !/^jim/.test(firstName || '') ? jimSurnames : [jimSurnames, lastName].filter(Boolean).join(', ')
+                    ), '')
+            }
+        }, inputObj), {
+            hendrix: 'jimmi',
+            carter: 'jimmy',
+            dean: 'james',
+            jims: 'morrison, hendrix, carter'
+        },
+        'creates the shape in the spec (just a take with fluent API using native JS)'
+    )
+    t.deepEqual(
+        shapeStrictly({morrison: 'Van', carter: pipe(head, concat(__, 'ohn'))}, inputObj),
+        {morrison: 'Van', carter: 'john'},
+        'only named props get through'
+    )
+    t.end()
+})
+
+test('"shape" can take a non-object as input', (t) => {
+    const numbers = [3, 4, 9, -3, 82, 274, 1334, 3, 13, 14, 47, 20]
+    t.deepEqual(shapeLoosely({sum, count: length}, numbers), {sum: 1800, count: 12})
+    t.end()
+})
+
+test('"shapeline" applies a list of functions (in sequence) to a single input', (t) => {
+    const numbers = [3, 4, 9, -3, 82, 274, 1334, 3, 13, 14, 47, 20]
+    const transforms = [
+        {sum, count: length},
+        s => (s.sum / (s.count || 1))
+    ]
+    t.deepEqual(shapeline(transforms, numbers), 150)
     t.end()
 })
