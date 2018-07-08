7e3e83c2a86eb803dae455620797c89c15970d74
David Nunez <arizonatribe@gmail.com>
2018-07-08 00:52:00 -0700

    Experimenting with a lighter build and with a shift to babel-preset-env. Referring to Redux's rollup config for some guidance on the topic. Fingers crossed

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
-import builtins from 'rollup-plugin-node-builtins'
 import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
 import uglify from 'rollup-plugin-uglify'
@@ -8,18 +7,30 @@ import {minify} from 'uglify-es'
 
 const env = process.env.NODE_ENV
 const config = {
-  output: {
-    format: 'umd',
-    name: 'shapey',
-    exports: 'named'
-  },
-  plugins: [
-    builtins(),
-    nodeResolve({jsnext: true}),
-    commonjs({include: 'node_modules/**'}),
-    babel({exclude: 'node_modules/**'}),
+  input: 'lib/index.js',
+  plugins: [nodeResolve({jsnext: true})]
+}
+
+if (env === 'es' || env === 'cjs') {
+  config.output = {format: env, indent: false}
+  config.external = ['ramda']
+  config.plugins.push(
+    babel({
+      plugins: ['external-helpers']
+    })
+  )
+}
+
+if (env === 'development' || env === 'production') {
+  config.output = {format: 'umd', name: 'shapey', indent: false}
+  config.plugins.push(
+    commonjs({include: 'node_modules/ramda/**'}),
+    babel({
+      exclude: 'node_modules/**',
+      plugins: ['external-helpers']
+    }),
     replace({'process.env.NODE_ENV': JSON.stringify(env)})
-  ]
+  )
 }
 
 if (env === 'production') {
